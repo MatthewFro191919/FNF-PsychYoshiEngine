89a21d0b892ca699b686f732e3eedf500fe87b51
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-06 15:59:40 -0500

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -7,7 +7,7 @@ echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp.git
 haxelib git lime https://github.com/mcagabe19-stuff/lime
 haxelib install openfl 9.2.2
-haxelib install flixel 5.2.2
+haxelib git flixel https://github.com/HaxeFlixel/flixel 57455528f5dc9c800b27f26b750830fc45bdeb32
 haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui 2.5.0
 haxelib install flixel-addons 2.10.0
