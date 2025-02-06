d953dfe51aef3000fde73cfda9e05dabb10c18f3
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-06 15:44:51 -0500

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -6,12 +6,24 @@ cd ..
 echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
-echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
-haxelib install flixel 5.6.1
-haxelib install flixel-addons 3.2.2
+echo This might take a few moments depending on your internet speed..
+haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp.git
+haxelib git lime https://github.com/mcagabe19-stuff/lime
+haxelib install openfl 9.2.2
+haxelib install flixel 5.2.2
 haxelib install flixel-tools 1.5.1
+haxelib install flixel-ui 2.5.0
+haxelib install flixel-addons 2.10.0
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib install hxcpp-debug-server
+haxelib install actuate
+haxelib install hxCodec 2.5.1
+haxelib install hscript
+haxelib git lime https://github.com/mcagabe19-stuff/lime
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
 haxelib install hxdiscord_rpc 1.2.4
